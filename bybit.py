07987ab754c58bedb1d10c766882d516bd27b3f4
Krystian <matematyk45@o2.pl>
2024-06-24 01:46:24 +0200

    checkpoint

--- bybit.py
+++ bybit.py
@@ -1,17 +1,39 @@
-import requests
-import websocket as ws
-import json
 from pybit.unified_trading import HTTP, WebSocket
 from ta.momentum import rsi
-import config
-import pandas
+import pandas as pd
+from time import sleep
+from collections import deque
+
+PERIOD = 50 # in timeframes - if timeframe is 60 then period is 1 hour
+TIMEFRAME = 1 #in minutes
+time = int(HTTP().get_server_time()['result']['timeSecond']) * 1000
+entry_data = HTTP().get_kline(category='spot', symbol='SOLUSDT', interval=TIMEFRAME, start=time-(TIMEFRAME*1000*60*PERIOD*4), end=time)
+dataset = deque(maxlen=PERIOD*4)
+
+for i in entry_data['result']['list']:
+    dataset.appendleft(i[4])
+
+print(rsi(close=pd.Series(data=dataset, dtype=float),window=PERIOD)[PERIOD*4-1])
 
 
-session = HTTP(testnet=False)
-print(session.get_kline(
-    category="inverse",
+def handler(x):
+    if x['data'][0]['confirm'] == True:
+        dataset.append(x['data'][0]['close'])
+        curr_RSI = (rsi(close=pd.Series(data=dataset, dtype=float),window=PERIOD)[PERIOD*4-1])
+        print(curr_RSI)
+
+
+
+
+session = WebSocket(channel_type='spot', testnet=False)
+session.kline_stream(
     symbol="SOLUSDT",
-    interval=60,
-    start=1670601600000,
-    end=1670608800000,
-))
+    interval=TIMEFRAME,
+    callback=handler)
+
+
+while True:     #for some reason necessary for pybit.unified_trading.WebSocket to work, otherwise program stops after 5-10 callbacks
+    sleep(1)
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord_interactions
 import config
-from random import randint, uniform
+from random import uniform
 import websocket
 from time import sleep
 import json
@@ -16,30 +16,20 @@ if __name__ == '__main__':
         """
         Function reacting to Gateway websocket connection messages
 
+        :param socket: The WebSocket connection the app is connected to
+        :param message: The messages received with the aforementioned connection
         """
 
         message = json.loads(message)
 
-        if message['op'] == 10:  #reaction to the hello message
-            heartbeat_interval = int(message['d']['heartbeat_interval']) / 1000
-            sleep(heartbeat_interval * uniform(0, 0.1))
-            socket.send(json.dumps({"op": 1, "d": message['s']}))
+        if message['t'] == 'INTERACTION_CREATE': #reacts to commands
+            interaction_id = message['d']['id']
+            interaction_token = message['d']['token']
+            command_name = message['d']['data']['name']
+            react_to_command(interaction_id=interaction_id, interaction_token=interaction_token, command=command_name)
 
-            sleep(1)
-
-            identify_message = json.dumps({
-                                'op': 2,
-                                'd': {
-                                    'token': config.TOKEN,
-                                    'properties': {
-                                        'os': 'windows',
-                                        'browser': 'stonks',
-                                        'device': 'stonks'},
-                                    'intents': 2048
-                                }})
-            socket.send(identify_message)
-
-
+        elif message['op'] == 10:  #reaction to the hello message
+            connect_to_discord(socket, message)
 
         elif message['op'] == 1:  #reaction to the heartbeat, aka ping
             socket.send(json.dumps({'op': 1, 'd': message['s']}))
@@ -61,8 +51,48 @@ if __name__ == '__main__':
                             "embeds": [{
                               "title": "Hello, Embed!",
                               "description": "This is an embedded message."}]}))
+        command_start_RSI()
+
+    def command_start_RSI():
+        url = "https://discord.com/api/v10/applications/{}/commands".format(config.CLIENT_ID)
+        json = {
+            "name": "relative_strength_index",
+            "type": 1,
+            "description": "Start calculating and sending RSI values for chosen pair, timeframe and period.",
+            "options": []}
+        headers = {"Authorization": "Bot {}".format(config.TOKEN)}
+        print(rq.post(url=url, json=json, headers=headers))
 
 
+    def react_to_command(interaction_id, interaction_token, command):
+        """
+        Function handling reacting to users' commands.
+        :param interaction_id: Interaction ID from the Gateway message
+        :param interaction_token: Interaction token from the Gateway message
+        :param command: For now doesn't do anything, but in the future can be used to send different data depending on the command used
+        :return:
+        """
+        url = "https://discord.com/api/v10/interactions/{}/{}/callback".format(interaction_id, interaction_token)
+        data = {
+            "type": 4,
+            "data": {
+                "content": "Congrats on sending your command!"}}
+        rq.post(url, json=data)
+
+    def connect_to_discord(socket, message):
+        heartbeat_interval = int(message['d']['heartbeat_interval']) / 1000
+        sleep(heartbeat_interval * uniform(0, 0.1))
+        socket.send(json.dumps({"op": 1, "d": message['s']}))
+        identify_message = json.dumps({
+            'op': 2,
+            'd': {
+                'token': config.TOKEN,
+                'properties': {
+                    'os': 'windows',
+                    'browser': 'stonks',
+                    'device': 'stonks'},
+                'intents': 8}})
+        socket.send(identify_message)
 
     t1 = threading.Thread(target=mainthread)
     t2 = threading.Thread(target=secondthread)
